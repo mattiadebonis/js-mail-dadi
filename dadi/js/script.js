4e277cf3a1f66dbdcf583259da08fbd94e2ac8c0
mattiadebonis <mattia.debonis@gmail.com>
2021-04-28 16:42:28 +0200

    logica js

---
+++ dadi/js/script.js
@@ -0,0 +1,11 @@
+// array registrate
+
+//chiedere all'utente la mail
+
+//variabile booleana per mail presente inizializzata false
+
+//itera array mail registrate
+
+// se mail array è uguale alla mail inserita variabile booleana allora true
+
+//restituisci risultato se vero allora messaggio positivo se falso messaggio negativo
